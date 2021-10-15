3de608e211e00ca07f27b749f032941e5da6b5f4
fred.fu <fjw08199999@gmail.com>
2021-10-15 11:23:20 +0800

    Initial Commit

---
+++ NineNineMultiplication/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NineNineMultiplication
+//
+//  Created by fred fu on 2021/10/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
